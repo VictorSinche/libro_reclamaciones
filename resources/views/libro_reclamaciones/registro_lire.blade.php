2e9190157d0a290afbd5effe586cc7865bf65c28
Tu Nombre <victorsinche>
2025-06-15 19:56:25 -0500

    d

--- resources/views/libro_reclamaciones/registro_lire.blade.php
+++ resources/views/libro_reclamaciones/registro_lire.blade.php
@@ -1,4 +1,10 @@
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+{{-- Solución alternativa: carga directa del CSS y JS compilados --}}
+@php
+    $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+@endphp
+
+<link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
+<script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
 
 
 <title>Libro de Reclamaciones - Universidad María Auxiliadora</title>
