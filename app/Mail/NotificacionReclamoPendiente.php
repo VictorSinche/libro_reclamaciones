0d4af1cd0af50002d923773cf971b96af8327c7b
VictorSinche <victor.sinche@uma.edu.pe>
2025-07-07 12:42:51 -0500

    Agregar notificación por correo para reclamos pendientes y programar su envío automático

---
+++ app/Mail/NotificacionReclamoPendiente.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Derivacion;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NotificacionReclamoPendiente extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $derivacion;
+
+    public function __construct(Derivacion $derivacion)
+    {
+        $this->derivacion = $derivacion;
+    }
+
+    public function build()
+    {
+        return $this->subject('🔔 Reclamo pendiente por atender')
+                    ->view('emails.notificacion_reclamo_pendiente');
+    }
+}
