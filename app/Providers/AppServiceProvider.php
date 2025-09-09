fbad53d6d2cb73038f68d631bc032fee9b929fed
VictorSinche <victor.sinche@uma.edu.pe>
2025-09-09 11:53:59 -0500

    d

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
+        if (env('FORCE_HTTPS') === 'production') {
             URL::forceScheme('https');
         }
     }
